5554f6d871e7dd3e870a4bf3db1868e2ba49df2a
Egg3234 <nospam@gmail.com>
2018-12-04 09:29:37 -0600

    asdf

--- hospital/src/hospital/GUI.java
+++ hospital/src/hospital/GUI.java
@@ -1,5 +1,12 @@
 package hospital;
-
+/*
+ * Samuel Giesbrecht
+ * Noah Voth
+ * 
+ * 12-04-18
+ * 
+ * First GUI class for hospital database program
+ */
 public class GUI {
 
 }
--- hospital/src/hospital/Hospital.java
+++ hospital/src/hospital/Hospital.java
@@ -1,9 +1,16 @@
 package hospital;
-
+/*
+ * Samuel Giesbrecht
+ * Noah Voth
+ * 
+ * 12-04-18
+ * 
+ * Main class for hospital database program
+ */
 public class Hospital {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
